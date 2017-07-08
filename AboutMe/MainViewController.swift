624326a3652214e449f52e7019345f37004c9d51
Edwin <epolo@wawand.co>
2017-07-08 09:08:24 -0500

    added Just dependency

---
+++ AboutMe/MainViewController.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
---
+++ Pods/Target Support Files/Pods-AboutMeTests/Pods-AboutMeTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_AboutMeTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AboutMeTestsVersionString[];
+
